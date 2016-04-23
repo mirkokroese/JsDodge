5021fee3f09c6b3711996258f546e67e006264c2
Mirko Kroese <mirkokroese@gmail.com>
2016-04-23 15:26:41 +0200

    added Enemy class

---
+++ assets/js/Enemy.js
@@ -0,0 +1,7 @@
+'use strict';
+    this.width = 100;
+    this.height = 100;
+    this.x = (document.body.clientWidth / 2) - this.width;
+    this.y = (document.body.clientHeight / 2) - this.height;
+    this.game = gameObject;
+};
